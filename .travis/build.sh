5ab0986da56b112c50501c3ab6ba652588c89ffc
Luc Murakami <luc.murakami@hotmail.com>
2020-06-12 01:25:38 -0700

    ecr push debug 2

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,12 +3,10 @@
 PRODUCTION_BRANCH="production"
 
 if [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
-  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
   aws configure set default.region us-west-2
 
-  eval $(aws ecr get-login -–no-include-email -–region us-west-2) 
-  docker build -t $DOCKER_IMAGE .
+  $(aws ecr get-login -–no-include-email) 
+  docker build --cache-from $ECR_REPO_NAME:latest -t $DOCKER_IMAGE .
   docker tag $DOCKER_IMAGE:latest $ECR_REPO_NAME:latest
   docker push $ECR_REPO_NAME:latest
 else
