c235573b87a2f2fdde04b053249042ab3fc4ba97
Luc Murakami <luc.murakami@hotmail.com>
2020-06-12 00:06:27 -0700

    Added to travis.yml

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,10 +3,14 @@
 PRODUCTION_BRANCH="production"
 
 if [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
-$(aws ecr get-login –no-include-email –region us-west-2) 
-docker build -t lucmurakami/lucmurakami-website-backend .
-docker tag lucmurakami/lucmurakami-website-backend:latest 164064335984.dkr.ecr.us-west-2.amazonaws.com/lucmurakami/lucmurakami-website-backend:latest
-docker push 164064335984.dkr.ecr.us-west-2.amazonaws.com/lucmurakami/lucmurakami-website-backend:latest
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+  aws configure set default.region us-west-2
+
+  $(aws ecr get-login –no-include-email –region us-west-2) 
+  docker build -t $DOCKER_IMAGE .
+  docker tag $DOCKER_IMAGE:latest $ECR_REPO_NAME:latest
+  docker push $ECR_REPO_NAME:latest
 else
   echo "Not deploying"
   exit
