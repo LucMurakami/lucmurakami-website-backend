5ad66f81a2ccfb6c925e2cada3002c094da81399
Luc Murakami <luc.murakami@hotmail.com>
2020-06-14 14:32:59 -0700

    added endpoint to test for travis build

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/test', (req, res) => {
   res.send({text:"Testing with travis"});
 });
 
+app.get('/help', (req, res) => {
+  res.send({text:"Help endpoint"});
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
