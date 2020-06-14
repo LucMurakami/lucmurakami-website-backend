82a843e080a0319cee8b650be6bf759dd12cbfc3
Luc Murakami <luc.murakami@hotmail.com>
2020-06-14 16:27:14 -0700

    Fixed error passing image name to ecs-deploy with multiparams

--- .travis/update-service.sh
+++ .travis/update-service.sh
@@ -1,7 +1,8 @@
 PRODUCTION_BRANCH="production"
 
 if [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
-  ./.travis/ecs-deploy.sh -c $ECS_CLUSTER -n $ECS_SERVICE -i $ECR_BASE_URI/$ECR_REPO_NAME:latest
+  ECR_IMAGE_NAME=$ECR_BASE_URI/$ECR_REPO_NAME:latest
+  ./.travis/ecs-deploy.sh -c $ECS_CLUSTER -n $ECS_SERVICE -i $ECR_IMAGE_NAME
 else
   echo "Not deploying"
   exit
