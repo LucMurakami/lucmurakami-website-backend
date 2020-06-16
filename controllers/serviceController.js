2af31b3712a77425b45859efd589bd78e5ad9053
Luc Murakami <luc.murakami@hotmail.com>
2020-06-16 12:59:43 -0700

    added motivation feature

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -12,6 +12,7 @@ exports.getMotivation = (req, res, next) => {
       title: data.data.children[0].data.title,
       image: data.data.children[0].data.preview.images[0].source.url,
       upvotes: data.data.children[0].data.ups,
+      permalink: data.data.children[0].data.permalink,
     }
     res.send(json)
   })
